dfa7481cbb204eabfce0899519b6c423cbe9de17
ezgibuse-akkaya <82088061+ezgibuse-akkaya@users.noreply.github.com>
2022-03-23 14:46:51 +0300

    Add project files.

---
+++ DataBinding/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataBinding
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string Cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(Cs))
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT CityId,CityName,Country FROM City", con);
+                    con.Open();
+                    DropDownList1.DataSource = cmd.ExecuteReader();
+                    //DropDownList1.DataTextField = "CityName";
+                    //DropDownList1.DataValueField = "CityId";
+                    DropDownList1.DataBind();
+
+                }
+            }
+        }
+    }
+
+
+}
